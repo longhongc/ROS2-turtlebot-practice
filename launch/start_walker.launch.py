d667cafe8532d1ac1af0a42de28acd2d40e32817
Chang-Hong Chen <longhongc@gmail.com>
2022-12-05 18:18:21 -0500

    Integrade nodes to launch

--- launch/start_walker.launch.py
+++ launch/start_walker.launch.py
@@ -1,11 +1,24 @@
 import os
 
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
+    record = LaunchConfiguration('record', default='false')
+
+    start_rosbag_record = ExecuteProcess(
+        cmd=['ros2', 'bag', 'record', '-a'],
+        output='screen',
+        condition=IfCondition(record)
+        )
+
     pkg_turtlebot3_gazebo = FindPackageShare(package='turtlebot3_gazebo'
         ).find('turtlebot3_gazebo')
 
@@ -15,8 +28,17 @@ def generate_launch_description():
                 'launch', 'turtlebot3_world.launch.py')
         ))
 
+    start_simple_walker = Node(
+        package='ros2_turtlebot_practice',
+        executable='simple_walker',
+        name='simple_walker_node',
+        output='screen',
+        )
+
     ld = LaunchDescription()
+    ld.add_action(start_rosbag_record)
     ld.add_action(start_turtlebot3_gazebo)
+    ld.add_action(start_simple_walker)
 
     return ld
 
